f175c7f266ff466e0fe5f451a9340395789cc8e4
Matheus Magno <matheusmagnon@gmail.com>
2022-07-25 15:03:44 -0300

    feat: add buttom for signOrder

--- src/components/OrderSent.tsx
+++ src/components/OrderSent.tsx
@@ -5,6 +5,8 @@ import styles from './OrderSent.module.css';
 import { ContainerForm } from './styled';
 import { Main } from './styled';
 
+import { WhatsappLogo } from 'phosphor-react';
+
 import Form from './Form';
 
 function OrderSent() {
@@ -22,14 +24,25 @@ function OrderSent() {
         <div className={styles.OrderSent}>
           <p>Sua solicitação foi registrada com sucesso!</p>
           <p>Iremos enviar os dados para pagamento através do WhatsApp ❤</p>
-          <input
-            type="submit"
-            className={styles.ButtonNewOrder}
-            value="Enviar Novo Pedido"
-            onClick={() => {
-              setOrderIsCompleted(true);
-            }}
-          />
+          <div className={styles.GroupBottons}>
+            <input
+              type="submit"
+              className={styles.ButtonNewOrder}
+              value="Enviar Novo Pedido"
+              onClick={() => {
+                setOrderIsCompleted(true);
+              }}
+            />
+
+            <a
+              className={styles.ButtonChatWhatsApp}
+              href=" https://api.whatsapp.com/send?phone=5563991069649&text=Oie, passando para avisar que fiz um pedido"
+              type="button"
+            >
+              <WhatsappLogo size={30} className={styles.WhatsappLogo} />
+              Sinalizar envio do pedido
+            </a>
+          </div>
         </div>
       </ContainerForm>
     </Main>
