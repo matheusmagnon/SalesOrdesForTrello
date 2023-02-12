f39582be2b8ac6eb6745d81012bf99a8545744b9
matheusmagnon@gmail.com <matheusmagnon@gmail.com>
2023-02-12 13:49:46 -0300

    change: horário sab e tamanho frase

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
-import * as yup from 'yup';
-import moment from 'moment';
+import * as yup from "yup";
+import moment from "moment";
 
 const IdBoard = import.meta.env.VITE_ID_BOARD;
 const IdListPedAberto = `624a047f2f06001532cef5e5`;
@@ -15,35 +15,35 @@ const Entrega = `624a04802f06001532cefe43`;
 const Retirada = `62a0c9dd4bed6367d6a3e17e`;
 
 const cardFields = {
-  idCustomNome: '6266f3cf7a05075d51e5d87a',
-  idCustomTel: '62844c42896c562dc00f4e72',
-  idCustomCor: '6266f4bca4f27866da085132',
-  idCustomVela: '6266f5496a22807bb41e25f3',
-  idCustomSabor: '627bbedb3290697b9e6e0ddf',
-  idCustomPagamento: '62845d1714150e5f18da882e',
+  idCustomNome: "6266f3cf7a05075d51e5d87a",
+  idCustomTel: "62844c42896c562dc00f4e72",
+  idCustomCor: "6266f4bca4f27866da085132",
+  idCustomVela: "6266f5496a22807bb41e25f3",
+  idCustomSabor: "627bbedb3290697b9e6e0ddf",
+  idCustomPagamento: "62845d1714150e5f18da882e",
 };
 
 const validationScheme = {
   nameInOrder: yup
     .string()
-    .required('Campo obrigatório')
-    .min(10, 'Por favor, escreva seu nome completo'),
-  celInOrder: yup.string().required('Campo obrigatório'),
+    .required("Campo obrigatório")
+    .min(10, "Por favor, escreva seu nome completo"),
+  celInOrder: yup.string().required("Campo obrigatório"),
   phraseOnTheCake: yup
     .string()
-    .required('Campo obrigatório')
-    .max(52, 'A quantidade de caracteres excede o espaço no bolo')
-    .min(8, 'Digite a frase que vai no Bolinho'),
-  cakePhraseColor: yup.string().required('Campo obrigatório'),
-  cakeColor: yup.string().required('Campo obrigatório'),
-  flavorInOrder: yup.string().required('Campo obrigatório').nullable(),
-  isWithdrawal: yup.string().required('Campo obrigatório').nullable(),
-  candleInOrder: yup.string().required('Campo obrigatório').nullable(),
-  formOfPaymentInOrder: yup.string().required('Campo obrigatório').nullable(),
+    .required("Campo obrigatório")
+    .max(35, "A quantidade de caracteres excede o espaço no bolo")
+    .min(6, "Digite a frase que vai no Bolinho"),
+  cakePhraseColor: yup.string().required("Campo obrigatório"),
+  cakeColor: yup.string().required("Campo obrigatório"),
+  flavorInOrder: yup.string().required("Campo obrigatório").nullable(),
+  isWithdrawal: yup.string().required("Campo obrigatório").nullable(),
+  candleInOrder: yup.string().required("Campo obrigatório").nullable(),
+  formOfPaymentInOrder: yup.string().required("Campo obrigatório").nullable(),
   dateTimeInOrder: yup
     .date()
-    .min(moment().add(10, 'm').toDate(), 'Selecione um horário válido de ')
-    .required('Campo obrigatório'),
+    .min(moment().add(10, "m").toDate(), "Selecione um horário válido de ")
+    .required("Campo obrigatório"),
 };
 
 export {
